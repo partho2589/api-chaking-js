8e048bf1841a805835611299cdbd7834721e44dc
partho2589 <parthomalo615@.com>
2022-02-28 13:01:30 +0600

    added meals

--- api.js
+++ api.js
@@ -1,5 +1,35 @@
 const loadSingLeUser = () => {
     fetch ('https://randomuser.me/api/')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displaySingleUser(data.results[0]))
+}
+loadSingLeUser()
+const displaySingleUser = user => {
+    // console.log(user)
+}
+
+const loadsMeals = searchText => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals))
+}
+loadsMeals('fish')
+
+const displayMeals = meals => {
+    console.log(meals)
+    const container = document.getElementById('meals')
+    meals.forEach ( meals => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <h1>${meals.strMeal}</h1>
+        <button onclick=" loadMealsDetails('${meals.strMeal}') "> click me</button>
+
+        `
+        container.appendChild(div)
+    })
+}
+
+const loadMealsDetails = mealName =>{
+    console.log(mealName)
 }
